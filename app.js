584d6adda096b938f673b98f32982ecb9e2466ca
mattser <jomatser@gmail.com>
2022-01-25 16:19:21 +0000

    Reworked JS Logic

--- app.js
+++ app.js
@@ -1,101 +1,122 @@
-let pushedNumber = 0;
-let workingNumber = 0;
-let globalOperator = "";
-let hasInitialValue = false;
+const calculator = {
+  buttons: document.querySelectorAll("button"),
+  workingLine: document.querySelector("p"),
+  answerLine: document.querySelector("h1"),
+  operator: "",
+  workingLineNumbers: [],
+  answerLineNumber: 0,
+  temporaryNumber: "",
+  justPush: false
+}
 
 const buttonPress = (button) => {
   button.addEventListener("click", (event) => {
+
     if (event.target.id.includes("num") || event.target.id==="decimal") {
-      appendNumberToDisplay(event.target.innerHTML);
+        // Pressing Number or Decimal will create an output on the working line
+        // It will also start adding to a temporary storage
+        appendValueToWorkingLine(event.target.innerHTML);
+        appendValueToTemporaryNumber(event.target.innerHTML);
     } else if (event.target.id==="clear") {
-      clearAll();
+        // Clears all variables
+        clearAll();
     } else if (event.target.id==="backspace") {
-      backspace();
+        // Removes the last character in Temporary Number
+        backspace();
     } else if (event.target.id==="percentage") {
 
-
     } else if (event.target.id==="calculate") {
-
-      workingNumber = document.querySelector("h1").innerHTML;
-      document.querySelector("p").innerHTML += ` ${workingNumber} =`;
-      document.querySelector("h1").innerHTML = calculate();
-      globalOperator = "";
-
+        // Pushes Temporary Number to working Number
+        // Takes Both working Numbers and performs the operation on them
+        pushTemporaryToWorking();
     } else {
-      
-      selectOperator(event.target.id);
-
-      if (!hasInitialValue) {
-        pushedNumber = document.querySelector("h1").innerHTML;
-        document.querySelector("p").innerHTML = `${pushedNumber} ${globalOperator}`;
-        document.querySelector("h1").innerHTML = "";
-        hasInitialValue = true;
-      } else {
-        workingNumber = document.querySelector("h1").innerHTML;
-        pushedNumber = calculate();
-        document.querySelector("p").innerHTML = `${pushedNumber} ${globalOperator}`;
-        document.querySelector("h1").innerHTML = ""
-        hasInitialValue = true;
-      }
-      console.log(workingNumber);
-      console.log(pushedNumber);
+        // Appends temporary number to working number. Clears the temporary Number
+        // If working Numbers already have 2 values calculate and reset the screen
+        // with new result and operator
+        if (calculator.temporaryNumber !=="") {
+          pushTemporaryToWorking();
+        }
+        appendValueToWorkingLine(event.target.innerHTML);
+        selectOperator(event.target.id);
     }
   });
 };
 
-const appendNumberToDisplay = (numberToAppend) => {
-  document.querySelector("h1").innerHTML += numberToAppend;
+// Appends any value to the working line
+const appendValueToWorkingLine = (valueToAppend) => {
+  calculator.workingLine.innerHTML += valueToAppend;
 }
-
+// Appends numbers and decimals to the current temporary storage
+const appendValueToTemporaryNumber = (valueToAppend) => {
+  calculator.temporaryNumber += valueToAppend;
+  console.log(calculator.temporaryNumber)
+}
+// Clears Screens and Variables
 const clearAll = () => {
-  document.querySelector("h1").innerHTML = "";
-  document.querySelector("p").innerHTML = "";
-  pushedNumber = 0;
-  workingNumber = 0;
-  globalOperator = "";
-  hasInitialValue = false;
+  calculator.workingLine.innerHTML = "";
+  calculator.answerLine.innerHTML = "";
+  calculator.operator = "";
+  calculator.workingLineNumbers = [];
+  calculator.answerLineNumber = 0;
+  calculator.temporaryNumber = "";
 }
 
-const backspace = () => {
-  document.querySelector("h1").innerHTML = document.querySelector("h1").innerHTML.slice(0,-1);
-}
-
-selectOperator = (operatorType) => {
-  switch(operatorType) {
-    case "addition":
-      globalOperator = "+";
-      break;
-    case "subtraction":
-      globalOperator = "-";
-      break;
-    case "multiplication":
-      globalOperator = "*";
-      break;
-    case "division":
-      globalOperator = "/";
-      break;
-  };
-}
-
-const calculate = () => {
+// Performs Calculation
+const performCalculation = () => {
   let result = 0;
-  switch(globalOperator) {
+  switch(calculator.operator) {
     case "+":
-      result = parseFloat(pushedNumber) + parseFloat(workingNumber);
+      result = calculator.workingLineNumbers[0] + calculator.workingLineNumbers[1];
       break;
     case "-":
-      result = parseFloat(pushedNumber) - parseFloat(workingNumber);
+      result = calculator.workingLineNumbers[0] - calculator.workingLineNumbers[1];
       break;
     case "*":
-      result = parseFloat(pushedNumber) * parseFloat(workingNumber);
+      result = calculator.workingLineNumbers[0] * calculator.workingLineNumbers[1];
       break;
     case "/":
-      result = parseFloat(pushedNumber) / parseFloat(workingNumber);
+      result = calculator.workingLineNumbers[0] / calculator.workingLineNumbers[1];
       break;
   }
-  console.log(`Result: ${pushedNumber} ${globalOperator} ${workingNumber}`);
-  hasInitialValue = false;
-  return result;
+  console.log(`Result: ${result}`);
+  calculator.answerLineNumber = result;
+  calculator.workingLineNumbers = [result];
+  calculator.answerLine.innerHTML = result;
 }
 
-document.querySelectorAll("button").forEach( (button) => buttonPress(button));
+// Selects the operator
+const selectOperator = (operatorType) => {
+  switch(operatorType) {
+    case "addition":
+      calculator.operator = "+";
+      break;
+    case "subtraction":
+      calculator.operator = "-";
+      break;
+    case "multiplication":
+      calculator.operator = "*";
+      break;
+    case "division":
+      calculator.operator = "/";
+      break;
+  };
+  console.log(calculator.operator)
+}
+
+// A function to push the current temporary value up to the working value;
+const pushTemporaryToWorking = () => {
+  console.log(calculator.temporaryNumber)
+  if (calculator.workingLineNumbers.length >= 1) {
+    console.log("Pushing To Second and Calculating");
+    calculator.workingLineNumbers[1] = parseFloat(calculator.temporaryNumber);
+    calculator.temporaryNumber = "";
+    performCalculation();
+  } else {
+    console.log("Pushing to First Slot");
+    calculator.workingLineNumbers = [parseFloat(calculator.temporaryNumber)];
+    calculator.temporaryNumber = "";
+  }
+  console.log(calculator.workingLineNumbers)
+}
+
+calculator.buttons.forEach( (button) => buttonPress(button));
---
+++ appold.js
@@ -0,0 +1,107 @@
+let pushedNumber = 0;
+let workingNumber = 0;
+let globalOperator = "";
+let hasInitialValue = false;
+
+const calculator = {
+  buttons: document.querySelectorAll("button"),
+  workingLine: document.querySelector("p").innerHTML,
+  answerLine: document.querySelector("h1").innerHTML
+}
+
+const buttonPress = (button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.id.includes("num") || event.target.id==="decimal") {
+      appendValueToWorkingLine(event.target.innerHTML);
+    } else if (event.target.id==="clear") {
+      clearAll();
+    } else if (event.target.id==="backspace") {
+      backspace();
+    } else if (event.target.id==="percentage") {
+
+
+    } else if (event.target.id==="calculate") {
+
+      workingNumber = document.querySelector("h1").innerHTML;
+      document.querySelector("p").innerHTML += ` ${workingNumber} =`;
+      document.querySelector("h1").innerHTML = calculate();
+      globalOperator = "";
+
+    } else {
+      
+      selectOperator(event.target.id);
+
+      if (!hasInitialValue) {
+        pushedNumber = document.querySelector("h1").innerHTML;
+        document.querySelector("p").innerHTML = `${pushedNumber} ${globalOperator}`;
+        document.querySelector("h1").innerHTML = "";
+        hasInitialValue = true;
+      } else {
+        workingNumber = document.querySelector("h1").innerHTML;
+        pushedNumber = calculate();
+        document.querySelector("p").innerHTML = `${pushedNumber} ${globalOperator}`;
+        document.querySelector("h1").innerHTML = ""
+        hasInitialValue = true;
+      }
+      console.log(workingNumber);
+      console.log(pushedNumber);
+    }
+  });
+};
+
+const appendValueToWorkingLine = (valueToAppend) => {
+  calculator.workingLine += valueToAppend;
+}
+
+const clearAll = () => {
+  calculator.answerLine = "";
+  calculator.workingLine = "";
+  pushedNumber = 0;
+  workingNumber = 0;
+  globalOperator = "";
+  hasInitialValue = false;
+}
+
+const backspace = () => {
+  document.querySelector("h1").innerHTML = document.querySelector("h1").innerHTML.slice(0,-1);
+}
+
+selectOperator = (operatorType) => {
+  switch(operatorType) {
+    case "addition":
+      globalOperator = "+";
+      break;
+    case "subtraction":
+      globalOperator = "-";
+      break;
+    case "multiplication":
+      globalOperator = "*";
+      break;
+    case "division":
+      globalOperator = "/";
+      break;
+  };
+}
+
+const calculate = () => {
+  let result = 0;
+  switch(globalOperator) {
+    case "+":
+      result = parseFloat(pushedNumber) + parseFloat(workingNumber);
+      break;
+    case "-":
+      result = parseFloat(pushedNumber) - parseFloat(workingNumber);
+      break;
+    case "*":
+      result = parseFloat(pushedNumber) * parseFloat(workingNumber);
+      break;
+    case "/":
+      result = parseFloat(pushedNumber) / parseFloat(workingNumber);
+      break;
+  }
+  console.log(`Result: ${pushedNumber} ${globalOperator} ${workingNumber}`);
+  hasInitialValue = false;
+  return result;
+}
+
+document.querySelectorAll("button").forEach( (button) => buttonPress(button));
